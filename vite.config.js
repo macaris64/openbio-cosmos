b4726772d03c2e9e3714a3793633a2fd87a93edb
macaris64 <mehmetacar683@gmail.com>
2025-10-05 15:10:37 +0300

    add vite

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: ".", // project root
+  publicDir: "public",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        atlas: "atlas.html",
+        article: "article.html",
+        experiments: "experiments.html",
+        geo: "geo.html",
+        "knowledge-atlas": "knowledge-atlas.html"
+      }
+    }
+  }
+});
