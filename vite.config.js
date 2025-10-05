d19473c399f4c48a2be49fe5405bff478e0235d7
macaris64 <mehmetacar683@gmail.com>
2025-10-05 15:19:29 +0300

    fix vercel deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         main: "index.html",
         atlas: "atlas.html",
         article: "article.html",
+        articles: "articles.html",
         experiments: "experiments.html",
         geo: "geo.html",
         "knowledge-atlas": "knowledge-atlas.html"
